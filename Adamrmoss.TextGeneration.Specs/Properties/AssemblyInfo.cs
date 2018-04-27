d0d770e25a471ef00d19dc8d19a70187666d35de
Adam R Moss <adamrmoss@gmail.com>
2018-04-26 22:14:26 -0400

    Update Metadata

--- Adamrmoss.TextGeneration.Specs/Properties/AssemblyInfo.cs
+++ Adamrmoss.TextGeneration.Specs/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TNW.TextGeneration.Specs")]
-[assembly: AssemblyDescription("Specs for TNW.TextGeneration")]
+[assembly: AssemblyTitle("Adamrmoss.TextGeneration.Specs")]
+[assembly: AssemblyDescription("Specs for Adamrmoss.TextGeneration")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("The Network, Inc.")]
-[assembly: AssemblyProduct("TNW.TextGeneration.Specs")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("Adam R Moss LLC")]
+[assembly: AssemblyProduct("Adamrmoss.TextGeneration")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.2")]
+[assembly: AssemblyFileVersion("1.2.2")]
